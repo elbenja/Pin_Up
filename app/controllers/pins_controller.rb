a1c5fab0e8c9e88370e39bc6fc317fd68189e08f
benjamin saravia <elbenja@gmail.com>
2015-03-12 01:01:37 -0400

    after styling

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
-	before_action :find_pin, only: [:show, :edit, :update, :destroy]
-
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
+	before_action :authenticate_user!, except:[:index,:show]
 	def index
 		@pins = Pin.all.order("created_at DESC" )
 	end
@@ -24,7 +24,12 @@ class PinsController < ApplicationController
 	end
 	def edit
 	end	
-	
+	def upvote
+
+		@pin.upvote_by current_user
+		redirect_to :back
+	end
+
 	def update
 		if @pin.update(pin_params)
 			redirect_to @pin, notice: "Pin was successfully updated"
